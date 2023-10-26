3e53a86885e193119e0ded2ac521bdea564cac1c
naoruki <72594954+naoruki@users.noreply.github.com>
2023-10-26 14:19:30 +0800

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HomeScreen from "./screens/HomeScreen";
+
 import {
 	createBrowserRouter,
 	createRoutesFromElements,
@@ -11,12 +11,15 @@ import {
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
 import App from "./App";
-
 import reportWebVitals from "./reportWebVitals";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<App />}>
 			<Route index={true} path="/" element={<HomeScreen />} />
+			<Route path="/product/:id" element={<ProductScreen />} />
 		</Route>
 	)
 );
