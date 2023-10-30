2245123bceaf7ac973ccf9f286d1ca0851420b5f
naoruki <72594954+naoruki@users.noreply.github.com>
2023-10-30 14:13:33 +0800

    commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,10 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Badge, Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
+import { useSelector } from "react-redux";
 import logo from "../assets/logo.png";
 const Header = () => {
+	const { cartItems } = useSelector((state) => state.cart);
 	return (
 		<header>
 			<Navbar bg="dark" variant="dark" expend="lg" collapseOnSelect>
@@ -20,6 +22,11 @@ const Header = () => {
 								<Nav.Link>
 									<FaShoppingCart />
 									Cart
+									{cartItems.length > 0 && (
+										<Badge pill bg="success" style={{ marginLeft: "5px" }}>
+											{cartItems.reduce((a, c) => a + c.qty, 0)}
+										</Badge>
+									)}
 								</Nav.Link>
 							</LinkContainer>
 							<LinkContainer to="/login">
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,15 +16,17 @@ import {
  import reportWebVitals from "./reportWebVitals";
  import HomeScreen from "./screens/HomeScreen";
  import ProductScreen from "./screens/ProductScreen";
+  import CartScreen from "./screens/CartScreen";
 
- const router = createBrowserRouter(
+	const router = createBrowserRouter(
 		createRoutesFromElements(
 			<Route path="/" element={<App />}>
 				<Route index={true} path="/" element={<HomeScreen />} />
 				<Route path="/product/:id" element={<ProductScreen />} />
+				<Route path="/cart" element={<CartScreen />} />
 			</Route>
 		)
- );
+	);
 
  const root = ReactDOM.createRoot(document.getElementById("root"));
  root.render(
