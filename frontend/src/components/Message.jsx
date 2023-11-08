cf9417f8718c66ded6270ba0c14a5abccf8d025e
naoruki <72594954+naoruki@users.noreply.github.com>
2023-11-08 23:16:49 +0800

    commit

--- frontend/src/components/Message.jsx
+++
@@ -1,9 +0,0 @@
-import { Alert } from "react-bootstrap";
-
-const Message = ({ variant, children }) => {
-	return <Alert variant={variant}>{children}</Alert>;
-};
-Message.deffaultProps = {
-	variant: "info",
-};
-export default Message;
