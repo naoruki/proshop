3a9b250ae86beccd0f6ef1d17680595d3422fd44
naoruki <72594954+naoruki@users.noreply.github.com>
2023-11-09 15:26:05 +0800

    commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,17 +3,19 @@ import Product from "../models/productModel.js";
 //@desc Fetch all products
 //@route GET /api/products
 //@access public
-const getProducts = asyncHandler(async (req, res, next) => {
-	const pageSize = 4;
+const getProducts = asyncHandler(async (req, res) => {
+	const pageSize = 8;
 	const page = Number(req.query.pageNumber) || 1;
-	const count = await Product.countDocuments();
+	//search for products
+	const keyword = req.query.keyword
+		? { name: { $regex: req.query.keyword, $options: "i" } }
+		: {};
+	const count = await Product.countDocuments({ ...keyword });
 
-	const products = await Product.find({})
+	const products = await Product.find({ ...keyword })
 		.limit(pageSize)
 		.skip(pageSize * (page - 1));
 	res.json({ products, page, pages: Math.ceil(count / pageSize) });
-	
-
 });
 //@desc Fetch a products
 //@route GET /api/products/:id
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import { Row, Col } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+
 import Product from "../components/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
@@ -7,10 +8,19 @@ import Paginate from "../components/Paginate";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 const HomeScreen = () => {
-	const { pageNumber } = useParams();
-	const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+	const { pageNumber, keyword } = useParams();
+	const { data, isLoading, error } = useGetProductsQuery({
+		keyword,
+		pageNumber,
+	});
 	return (
 		<>
+			{keyword && (
+				<Link to="/" className="btn btn-light mb-4">
+					{" "}
+					Go Back
+				</Link>
+			)}
 			{isLoading ? (
 				<Loader />
 			) : error ? (
@@ -27,7 +37,11 @@ const HomeScreen = () => {
 							</Col>
 						))}
 					</Row>
-					<Paginate pages={data.pages} page={data.page} />
+					<Paginate
+						pages={data.pages}
+						page={data.page}
+						keyword={keyword ? keyword : ""}
+					/>
 				</>
 			)}
 		</>
